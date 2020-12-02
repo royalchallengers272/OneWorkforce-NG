03210a062330b1aded3f96081c82aaabf8c5912b
Varun Selvakumar <vselvaku@cisco.com>
2020-12-02 15:06:15 +0530

    build issue

--- src/app/employee-management/directory/directory.component.ts
+++ src/app/employee-management/directory/directory.component.ts
@@ -7,6 +7,14 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DirectoryComponent implements OnInit {
   @Input() isManagerOut:any;
+  rowData:[{}];
+  columnDefs = [
+    { field: 'emp_no',sortable: true, filter: true },
+    { field: 'birth_date',sortable: true, filter: true },
+    { field: 'first_name',sortable: true, filter: true},
+    { field: 'last_name',sortable: true, filter: true},
+    { field: 'hire_date',sortable: true, filter: true}
+];
   constructor() { }
 
   ngOnInit() {
