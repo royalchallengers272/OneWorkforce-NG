b952e4eda9a05d2edc8bc7a8a4e6ddaeaed3e6ca
Varun Selvakumar <vselvaku@cisco.com>
2020-12-03 15:20:06 +0530

    UI changes

--- src/app/employee-management/directory/directory.component.ts
+++ src/app/employee-management/directory/directory.component.ts
@@ -9,24 +9,26 @@ export class DirectoryComponent implements OnInit {
   @Input() isManagerOut:any;
   rowData:any;
   columnDefs = [
-    { field: 'emp_no',sortable: true, filter: true },
-    { field: 'birth_date',sortable: true, filter: true },
-    { field: 'first_name',sortable: true, filter: true},
-    { field: 'last_name',sortable: true, filter: true},
-    { field: 'hire_date',sortable: true, filter: true},
-    { field: 'gender',sortable: true, filter: true},
-    { field: 'email',sortable: true, filter: true}
+    { headerName: 'Employee number',field: 'emp_no',sortable: true, filter: true },
+    { headerName: 'First Name',field: 'first_name',sortable: true, filter: true },
+    { headerName: 'Last Name',field: 'last_name',sortable: true, filter: true},
+    { headerName: 'Email',field: 'email',sortable: true, filter: true},
+    { headerName: 'Manager',field: 'managername',sortable: true, filter: true},
+    { headerName: 'Phone',field: 'city',sortable: true, filter: true},
+    { headerName: 'Department',field: 'state',sortable: true, filter: true}
 ];
   empOfManager: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any>('http://ec2-52-53-164-188.us-west-1.compute.amazonaws.com:8080/ONEWORKFORCE/api/getAllEmployeeDetails').subscribe(data => {
+    if(this.isManagerOut){
+      this.http.get<any>('http://ec2-52-53-164-188.us-west-1.compute.amazonaws.com:8080/ONEWORKFORCE/api/getManagerEmployeeDetails?empid=10001').subscribe(data => {
             this.rowData=data;
-    })
-    this.http.get<any>('http://ec2-52-53-164-188.us-west-1.compute.amazonaws.com:8080/ONEWORKFORCE/api/getManagerEmployeeDetails?empid=10001').subscribe(data => {
-            this.empOfManager = data.map(function (el) { return el.first_name+" "+el.last_name; });
-    })
+      })
+    }
+    // this.http.get<any>('http://ec2-52-53-164-188.us-west-1.compute.amazonaws.com:8080/ONEWORKFORCE/api/getManagerEmployeeDetails?empid=10001').subscribe(data => {
+    //         this.empOfManager = data.map(function (el) { return el.first_name+" "+el.last_name; });
+    // })
   }
 
 }
